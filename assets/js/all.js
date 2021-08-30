56446c808093f32f540cc4df9ec42232c2192e56
valerie300 <zxcv841219595@gmail.com>
2021-08-30 10:23:27 +0800

    Update 2021-08-30T02:23:21.109Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,91 +1,98 @@
+"use strict";
+
 (function () {
-  "use strict";
-  // breakpoint where swiper will be destroyed
+  "use strict"; // breakpoint where swiper will be destroyed
   // and switches to a dual-column layout
-  const breakpoint = window.matchMedia("(max-width: 992px)");
-  // keep track of swiper instances to destroy later
-  let mySwiper;
-  const breakpointChecker = function () {
+
+  var breakpoint = window.matchMedia("(max-width: 992px)"); // keep track of swiper instances to destroy later
+
+  var mySwiper;
+
+  var breakpointChecker = function breakpointChecker() {
     // if larger viewport and multi-row layout needed
     if (breakpoint.matches === true) {
       // clean up old instances and inline styles when available
-      if (mySwiper !== undefined) mySwiper.destroy(true, true);
-      // or/and do nothing
-      return;
-      // else if a small viewport and single column layout needed
+      if (mySwiper !== undefined) mySwiper.destroy(true, true); // or/and do nothing
+
+      return; // else if a small viewport and single column layout needed
     } else if (breakpoint.matches === false) {
       // fire small viewport version of swiper
       return enableSwiper();
     }
   };
-  const enableSwiper = function () {
+
+  var enableSwiper = function enableSwiper() {
     mySwiper = new Swiper(".swiper-container", {
       slidesPerView: 1.5,
-      spaceBetween: 30,
+      spaceBetween: 30
     });
-  };
-  // keep an eye on viewport size changes
-  breakpoint.addListener(breakpointChecker);
-  // kickstart
+  }; // keep an eye on viewport size changes
+
+
+  breakpoint.addListener(breakpointChecker); // kickstart
+
   breakpointChecker();
-})(); /* IIFE end */
+})();
+/* IIFE end */
+
 
 var usswiper = new Swiper(".swiper-body", {
   slidesPerView: 2,
-  spaceBetween: 30,
-});
+  spaceBetween: 30
+}); //日期
 
-//日期
 $(function () {
   $("#datepicker").datepicker({
-    dateFormat: "mm-dd-yy", //修改顯示順序
-  });
-});
+    dateFormat: "mm-dd-yy" //修改顯示順序
 
-//師資
+  });
+}); //師資
+
 var weswiper = new Swiper(".weSwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   breakpoints: {
     375: {
       slidesPerView: 1,
-      spaceBetween: 50,
+      spaceBetween: 50
     },
     768: {
       slidesPerView: 2,
-      spaceBetween: 50,
+      spaceBetween: 50
     },
     960: {
       slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-});
-// 課表
+      spaceBetween: 30
+    }
+  }
+}); // 課表
+
 var listswiper = new Swiper(".listSwiper", {
   slidesPerView: 1,
   spaceBetween: 0,
-  observer: true, //修改swiper自己或子元素時，自動初始化swiper
-  observeParents: true, //修改swiper的父元素時，自動初始化swiper
+  observer: true,
+  //修改swiper自己或子元素時，自動初始化swiper
+  observeParents: true,
+  //修改swiper的父元素時，自動初始化swiper
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 3,
-      spaceBetween: 20,
+      spaceBetween: 20
     },
     // when window width is >= 480px
     768: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 30
     },
     // when window width is >= 640px
     960: {
       slidesPerView: 1,
-      spaceBetween: 0,
-    },
-  },
-});
-// 課程推薦
+      spaceBetween: 0
+    }
+  }
+}); // 課程推薦
+
 var recommendSwiper = new Swiper(".Swiper-recommend", {
   slidesPerView: 4,
   spaceBetween: 30,
@@ -93,17 +100,18 @@ var recommendSwiper = new Swiper(".Swiper-recommend", {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 0,
+      spaceBetween: 0
     },
     // when window width is >= 480px
     768: {
       slidesPerView: 3,
-      spaceBetween: 30,
+      spaceBetween: 30
     },
     // when window width is >= 640px
     960: {
       slidesPerView: 4,
-      spaceBetween: 30,
-    },
-  },
+      spaceBetween: 30
+    }
+  }
 });
+//# sourceMappingURL=all.js.map
